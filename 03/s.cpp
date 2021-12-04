dbaf2fe1eb36f8725cf3bb8667aa3d070efd9021
Boris <boris.kuznetsov@native-instruments.de>
2021-12-04 23:55:05 +0100

    day 3

---
+++ 03/s.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <bitset>
+#include <numeric>
+#include <functional>
+#include <algorithm>
+#include "input.h"
+
+enum class Rating
+{
+    CO2 = 0,
+    Oxygen = 1
+};
+
+auto get_most_used_bit(auto bitset, int bit) -> bool
+{
+    const auto num_ones{std::transform_reduce(bitset.cbegin()
+                                , bitset.cend()
+                                , 0
+                                , std::plus<>()
+                                , [bit](auto set){return static_cast<int>(set[bit]);})};
+    const auto num_zeros{bitset.size() - num_ones};
+    return num_ones >= num_zeros;
+}
+
+template<typename v_t>
+auto part1(v_t binary_input) -> int
+{
+    typename v_t::value_type gamma;
+    typename v_t::value_type epsilon;
+
+    for(unsigned bit{0}; bit < binary_input[0].size(); ++bit)
+    {
+        auto bit_to_set {get_most_used_bit(binary_input, bit)};
+        gamma[bit] = bit_to_set;
+    }
+
+    epsilon = gamma;
+    epsilon.flip();
+
+    return gamma.to_ulong() * epsilon.to_ulong();
+}
+
+template<typename v_t>
+auto part2(v_t input, unsigned bit_to_check, Rating mode) -> v_t
+{
+    if(input.size() == 2)
+    {
+        v_t return_vector;
+        std::copy_if(input.cbegin()
+                    , input.cend()
+                    , std::back_inserter(return_vector)
+                    , [bit_to_check, mode](auto word){return word[bit_to_check] == bool(mode);});
+
+        return return_vector;
+    }
+
+    //fallthrough
+    int target_bit {get_most_used_bit(input, bit_to_check)};
+
+    v_t return_vector;
+    std::copy_if(input.cbegin()
+                , input.cend()
+                , std::back_inserter(return_vector)
+                , [target_bit, bit_to_check, mode](auto word){return (word[bit_to_check] == target_bit) == bool(mode);});
+
+    return part2(return_vector, bit_to_check - 1, mode);
+}
+
+int main()
+{
+    std::cout << "Part 1: " << part1(advent03::input) << "\n";
+
+    auto oxygen_rating{part2(advent03::input, advent03::input[0].size() - 1, Rating::Oxygen)};
+    auto co2_rating{part2(advent03::input, advent03::input[0].size() - 1, Rating::CO2)};
+
+    std::cout << "Part 2: " << oxygen_rating[0].to_ulong() * co2_rating[0].to_ulong() << "\n";
+}
